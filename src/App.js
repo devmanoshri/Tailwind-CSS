20e8342c97284692162531b169af64a84ba847ee
devmanoshri <manoshri.no@gmail.com>
2022-12-31 20:31:58 +0100

    update 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React from "react";
 import Button from "./Button";
+import { GoBell, GoDatabase, GoCloudDownload } from "react-icons/go";
 function App() {
+  const handelClick = () => {
+    console.log("Click");
+  };
   return (
     <div>
       <div>
-        <Button primary>Click Me!</Button>
+        <Button primary rounded className="mb-5" onMouseEnter={handelClick}>
+          <GoBell />
+          Click Me!
+        </Button>
       </div>
       <div>
-        <Button secondary>Buy Now</Button>
+        <Button secondary outline rounded onClick={handelClick}>
+          <GoDatabase />
+          Buy Now
+        </Button>
       </div>
       <div>
-        <Button success>See ya</Button>
+        <Button success>
+          <GoCloudDownload />
+          See ya
+        </Button>
       </div>
       <div>
-        <Button warning>See Deal</Button>
+        <Button warning outline>
+          See Deal
+        </Button>
       </div>
       <div>
-        <Button danger>Hide Ads</Button>
+        <Button danger rounded>
+          Hide Ads
+        </Button>
       </div>
     </div>
   );
