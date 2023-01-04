2f670961e305807c79d517869be2bc7220594ab3
devmanoshri <manoshri.no@gmail.com>
2023-01-04 09:38:13 +0100

    dropdown 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 function App() {
+  const [selection, setSelection] = useState(null);
+  const handelSelection = (option) => {
+    setSelection(option);
+  };
   const dropdownOptions = [
     { label: "Red", value: "Red" },
     { label: "Green", value: "Green" },
     { label: "Blue", value: "Blue" },
     { label: "Yellow", value: "Yellow" },
   ];
-  return <Dropdown options={dropdownOptions} />;
+  return (
+    <Dropdown
+      options={dropdownOptions}
+      value={selection}
+      onChange={handelSelection}
+    />
+  );
 }
 
 export default App;
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,13 +1,29 @@
 import { useState } from "react";
 
-function Dropdown({ options }) {
+function Dropdown({ options, value, onChange }) {
+  const [showOption, setShowOption] = useState(false);
+
+  const handelShowOption = () => {
+    setShowOption(!showOption);
+  };
+  const handelOptionClick = (option) => {
+    //console.log(option);
+    setShowOption(false);
+    onChange(option);
+  };
   const renderedData = options.map((option) => {
-    return <div>{option.value}</div>;
+    return (
+      <div key={option.value} onClick={() => handelOptionClick(option)}>
+        {option.label}
+      </div>
+    );
   });
+
+  //let content = selection === null ? "Select..." : selection.value;
   return (
     <div>
-      <div>Select...</div>
-      <div>{renderedData}</div>
+      <div onClick={handelShowOption}>{value?.label || "Select..."}</div>
+      {showOption && <div>{renderedData}</div>}
     </div>
   );
 }
