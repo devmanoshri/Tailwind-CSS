45c39cd3c2f90846f72abd4becd98263970701ac
devmanoshri <manoshri.no@gmail.com>
2023-01-10 11:31:06 +0100

    modal basic 2

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import Modal from "../components/Modal";
+import Button from "../components/Button";
 
 function ModalPage() {
-  return <Modal />;
+  const [showModal, setShowModal] = useState(false);
+  const handelModalShow = () => {
+    setShowModal(true);
+  };
+  const handelClose = () => {
+    setShowModal(false);
+  };
+  return (
+    <div>
+      <Button primary onClick={handelModalShow}>
+        Open Modal
+      </Button>
+      {showModal && <Modal onClose={handelClose} />}
+    </div>
+  );
 }
 
 export default ModalPage;
