51d2dc5b1d84bb176ee855573dfbe2c987764050
devmanoshri <manoshri.no@gmail.com>
2023-01-18 16:03:24 +0100

    counter

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,40 +1,58 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
-function CounterPage({ initialValue }) {
-  const [count, setCount] = useState(10);
-  const [valueToAdd, setValueToAdd] = useState(0);
+const reducer = (state, action) => {
+  return {
+    ...state,
+    count: state.count + 1,
+  };
+};
+
+function CounterPage({ initialCount }) {
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+  console.log(state);
+
   const increment = () => {
-    setCount(count + 1);
+    dispatch();
   };
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
   };
-  const handelChange = (event) => {
+  const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    setValueToAdd(value);
+
+    // setValueToAdd(value);
   };
-  const handelSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0)
+
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
+
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is: {count}</h1>
+      <h1 className="text-lg">Count is {state.count}</h1>
       <div className="flex flex-row">
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
       </div>
-      <form onSubmit={handelSubmit}>
+
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
-          value={valueToAdd || ""}
-          onChange={handelChange}
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
         />
+        <Button>Add it!</Button>
       </form>
     </Panel>
   );
